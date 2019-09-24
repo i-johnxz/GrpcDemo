0cd457a29904899ab5826d2a760e188702776f76
xiongz_home <xiongz@DESKTOP-SVDIFJE>
2019-09-24 23:24:05 +0800

    update code

---
+++ grpc/GrpcDemo/GrpcClientNestedTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcServerNestedTypes.Protos;
+
+namespace GrpcClientNestedTypes
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //We need this switch because we are connecting to an unsecure server. If the server runs on SSL, there's no need for this switch.
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Board.BoardClient(channel);
+            var request = new MessageRequest();
+            request.Capabilities.Add(new MessageRequest.Types.SuperPower {Name = "Flying", Level = 1});
+            request.Capabilities.Add(new MessageRequest.Types.SuperPower {Name = "Invisibility", Level = 10});
+            request.Capabilities.Add(new MessageRequest.Types.SuperPower {Name = "Speed", Level = 5});
+
+            var reply = await client.ShowMessageAsync(request);
+
+            var displayDate = new DateTime(reply.ReceivedTime);
+            Console.WriteLine(
+                $"We sent a message to a gRPC server and  received  the following reply \n'\n{reply.Message}' \non {displayDate} ");
+            Console.WriteLine("End");
+            Console.Read();
+        }
+    }
+}
---
+++ grpc/GrpcDemo/GrpcServerNestedTypes/BillboardService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using GrpcServerNestedTypes.Protos;
+
+namespace GrpcServerNestedTypes
+{
+    public class BillboardService : Board.BoardBase
+    {
+        public override Task<MessageReply> ShowMessage(MessageRequest request, ServerCallContext context)
+        {
+            var message = new StringBuilder();
+            foreach (var c in request.Capabilities)
+            {
+                message.AppendLine($"{c.Name} level {c.Level}");
+            }
+
+            var now = DateTime.UtcNow;
+            return Task.FromResult(new MessageReply
+            {
+                ReceivedTime = now.Ticks,
+                Message = message.ToString()
+            });
+
+        }
+    }
+}
