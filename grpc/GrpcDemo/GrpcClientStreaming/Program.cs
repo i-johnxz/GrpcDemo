6e2895fbee5a20580e3cb44463855f9da638b738
xiongz <a824440331@gmail.com>
2019-09-24 16:45:37 +0800

    update code

---
+++ grpc/GrpcDemo/GrpcClientStreaming/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer.Protos;
+
+namespace GrpcClientStreaming
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //We need this switch because we are connecting to an unsecure server. If the server runs on SSL, there's no need for this switch.
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Board.BoardClient(channel);
+            var result = client.ShowMessage(new MessageRequest
+            {
+                Name = "Johny"
+            });
+
+            using var tokenSource = new CancellationTokenSource();
+            CancellationToken token = tokenSource.Token;
+
+            var streamReader = result.ResponseStream;
+
+            await foreach (var reply in streamReader.ReadAllAsync(token))
+            {
+                var displayDate = new DateTime(reply.DisplayTime);
+                Console.WriteLine($"Received \"{reply.Message}\" on {displayDate.ToLongTimeString()} \n");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ grpc/GrpcDemo/GrpcServerStreaming/BillboardService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using GrpcServer.Protos;
+
+namespace GrpcServerStreaming
+{
+    public class BillboardService : Board.BoardBase
+    {
+        public override async Task ShowMessage(MessageRequest request, IServerStreamWriter<MessageReply> responseStream, ServerCallContext context)
+        {
+            foreach (var x in Enumerable.Range(1, 10))
+            {
+                var now = DateTime.UtcNow;
+
+                await responseStream.WriteAsync(new MessageReply
+                {
+                    DisplayTime = now.Ticks,
+                    Message = $"Hello {request.Name}"
+                });
+
+                await Task.Delay(5000);
+            }
+        }
+    }
+}
