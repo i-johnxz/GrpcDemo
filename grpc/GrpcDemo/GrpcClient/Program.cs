79ec308097380dacf1675e727679443cabcb8d3b
xiongz <a824440331@gmail.com>
2019-09-24 16:17:14 +0800

    add project

---
+++ grpc/GrpcDemo/GrpcClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcServer.Protos;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //We need this switch because we are connecting to an unsecure server. If the server runs on SSL, there's no need for this switch.
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Board.BoardClient(channel);
+            var reply = await client.ShowMessageAsync(new MessageRequest
+            {
+                Message = "Good morning people of the world",
+                Sender = "Dody Gunawinata"
+            });
+
+            var displayDate = new DateTime(reply.DisplayTime);
+            Console.WriteLine($"This server sends a gRPC request to a server and get the following result: Received message on {displayDate} from {reply.ReceiveFrom}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ grpc/GrpcDemo/GrpcServer/BillboardService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using GrpcServer.Protos;
+
+namespace GrpcServer
+{
+    public class BillboardService : Board.BoardBase
+    {
+        public override Task<MessageReply> ShowMessage(MessageRequest request, ServerCallContext context)
+        {
+            var now = DateTime.UtcNow;
+            return Task.FromResult(new MessageReply
+            {
+                DisplayTime = now.Ticks,
+                ReceiveFrom = request.Sender
+            });
+        }
+    }
+}
