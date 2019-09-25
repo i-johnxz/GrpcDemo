83672c49f9ca2f26fbb677384e7d38f9b6fc0ad3
xiongz <a824440331@gmail.com>
2019-09-25 10:12:25 +0800

    update code

---
+++ grpc/GrpcDemo/GrpcClientDictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcServerDictionary.Protos;
+
+namespace GrpcClientDictionary
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Board.BoardClient(channel);
+            var request = new MessageRequest();
+            request.Capabilities.Add("fly", new MessageRequest.Types.SuperPower { Name = "Flying", Level = 1 });
+            request.Capabilities.Add("inv", new MessageRequest.Types.SuperPower { Name = "Invisibility", Level = 10 });
+            request.Capabilities.Add("spe", new MessageRequest.Types.SuperPower { Name = "Speed", Level = 5 });
+
+            var reply = await client.ShowMessageAsync(request);
+
+            var displayDate = new DateTime(reply.ReceivedTime);
+            Console.WriteLine(
+                $"We sent a message to a gRPC server and  received  the following reply \n'\n{reply.Message}' \non {displayDate} ");
+            Console.WriteLine("End");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ grpc/GrpcDemo/GrpcServerDictionary/BillboardService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using GrpcServerDictionary.Protos;
+
+namespace GrpcServerDictionary
+{
+    public class BillboardService : Board.BoardBase
+    {
+        public override Task<MessageReply> ShowMessage(MessageRequest request, ServerCallContext context)
+        {
+            var message = new StringBuilder();
+            foreach (var (k, c) in request.Capabilities)
+            {
+                message.AppendLine($"'{k}' = {c.Name} level {c.Level}");
+            }
+
+            var now = DateTime.UtcNow;
+            return Task.FromResult(new MessageReply
+            {
+                ReceivedTime = now.Ticks,
+                Message = message.ToString()
+            });
+        }
+    }
+}
