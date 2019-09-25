710d0826d81d05b38c01d648e25dfefc14acd46f
xiongz <a824440331@gmail.com>
2019-09-25 10:31:33 +0800

    update code

---
+++ grpc/GrpcDemo/GrpcClientSimulateNullableValue/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcServerSimulateNullableValue.Protos;
+
+namespace GrpcClientSimulateNullableValue
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+
+            var client = new Board.BoardClient(channel);
+            var request = new MessageRequest();
+            request.WageValue = 10_000;
+            request.BonusValue = 9_000;
+
+            var reply = await client.ShowMessageAsync(request);
+
+            var displayDate = new DateTime(reply.ReceivedTime);
+            Console.WriteLine($"We sent a message to a gRPC server and  received  the following reply \n'\n{reply.Message}' \non {displayDate} ");
+            Console.WriteLine("End");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ grpc/GrpcDemo/GrpcServerSimulateNullableValue/BillboardService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using GrpcServerSimulateNullableValue.Protos;
+
+namespace GrpcServerSimulateNullableValue
+{
+    public class BillboardService : Board.BoardBase
+    {
+        public override Task<MessageReply> ShowMessage(MessageRequest request, ServerCallContext context)
+        {
+            var message = new StringBuilder();
+            message.AppendLine($"WageCase {request.WageCase}");
+            message.AppendLine($"WageValue {request.WageValue}");
+            message.AppendLine($"BonusCase {request.BonusCase}");
+            if (request.BonusCase == MessageRequest.BonusOneofCase.None)
+            {
+                message.AppendLine(
+                    "BonusCase None means that BonusValue property is never set by the requester. So you can ignore whatever values you see at BonusValue property.");
+            }
+
+            message.AppendLine($"BonusValue {request.BonusValue}");
+
+            var now = DateTime.UtcNow;
+            return Task.FromResult(new MessageReply
+            {
+                ReceivedTime = now.Ticks,
+                Message = message.ToString()
+            });
+        }
+    }
+}
