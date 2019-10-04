2c57ff859feac5fcf7d76afbf128ff047bca63b3
xiongz_home <xiongz@DESKTOP-SVDIFJE>
2019-10-04 14:59:44 +0800

    update code

---
+++ grpc/GrpcDemo/SampleProject.Grpc.Api/CustomerOrdersService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using SampleProject.Grpc.Api.Protos;
+
+namespace SampleProject.Grpc.Api
+{
+    public class CustomerOrdersService : Protos.CustomerOrdersService.CustomerOrdersServiceBase
+    {
+        public override Task<OrderDtos> GetCustomerOrders(GetCusomerOrderRequest request, ServerCallContext context)
+        {
+            return base.GetCustomerOrders(request, context);
+        }
+
+        public override Task<OrderDetailsDto> GetCustomerOrderDetails(GetCustomerOrderDetailsRequest request, ServerCallContext context)
+        {
+            return base.GetCustomerOrderDetails(request, context);
+        }
+
+        public override Task<AddCustomerOrderResult> AddCustomerOrder(AddCustomerOrderRequest request, ServerCallContext context)
+        {
+            return base.AddCustomerOrder(request, context);
+        }
+
+        public override Task<ChangeCustomerOrderResult> ChangeCustomerOrder(ChangeCustomerOrderRequest request, ServerCallContext context)
+        {
+            return base.ChangeCustomerOrder(request, context);
+        }
+
+        public override Task<RemoveCustomerOrderResult> RemoveCustomerOrder(RemoveCustomerOrderRequest request, ServerCallContext context)
+        {
+            return base.RemoveCustomerOrder(request, context);
+        }
+    }
+
+}
---
+++ grpc/GrpcDemo/SampleProject.Grpc.Api/CustomersService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using SampleProject.Grpc.Api.Protos;
+
+namespace SampleProject.Grpc.Api
+{
+    public class CustomersService : CustomerService.CustomerServiceBase
+    {
+        
+
+        public override Task<CustomerDto> RegisterCustomer(RegisterCustomerRequest request, ServerCallContext context)
+        {
+            return base.RegisterCustomer(request, context);
+        }
+    }
+}
